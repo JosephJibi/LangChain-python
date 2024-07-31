c1dea722ef8652e9290afd783763be0b2bc673b0
Jibi Joseph <jibi.joseph@vonnue.com>
2024-07-31 18:12:43 +0530

    feat: add basic lang chain implementation

--- llm.py
+++ llm.py
@@ -1,10 +1,15 @@
-from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-llm = ChatOpenAI(
-    api_key="sk-proj-lRiarById1qINSPJOT9cT3BlbkFJCMgsTpTidoispK0xuhaq",
-    model="gpt-3.5-turbo"
-)
+load_dotenv()
 
-response = llm.invoke("Hello, How are you?")
+llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7, verbose=True)
+# response = llm.invoke("Hello, how are you?") # Simple way to ask questions.
+# response = llm.batch(["Hello, how are  you?", "Example use of linear algebra in ML"]) # We can give array of messages, all of this messages is processed concurrently.
 
-print(response)
+# print(response)
+
+# To Get Streaming response.
+responseChunks = llm.stream("write a poem about AI")
+for chunk in responseChunks:
+        print(chunk.content, end="", flush=True)
