403fca8642b8ab03165892896eac1359ee36bb61
Jibi Joseph <jibi.joseph@vonnue.com>
2024-08-01 19:27:41 +0530

    feat: add output parser example

--- promptTemplatesTwo.py
+++ promptTemplatesTwo.py
@@ -27,7 +27,8 @@ print(response.content)
 
 
 # Note:
-# GenAI model like: "", "" - don't  support SystemMessage directly(from_messages in this case).
+# GenAI model like: "gemini-pro", "gemini-flash" - don't  support SystemMessage directly(from_messages in this case).
 # So, we can use 'convert_system_message_to_human=True'
 # This will add the system message to the first human message in the sequence.
 # Reference: Link -  https://github.com/langchain-ai/langchain/discussions/24817
+# Update on this issue(on Aug 1, 2024) -  we can use modal 'gemini-1.5-pro-latest' to fix this issue. it the current mdoal.
